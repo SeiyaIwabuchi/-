513688131c78f1868e71569eb82b4942d85df583
SeiyaIwabuchi <0123ook@gmail.com>
2020-09-18 18:47:32 +0900

    update

--- BasicOTA2.ino/BasicOTA2.ino.ino
+++ BasicOTA2.ino/BasicOTA2.ino.ino
@@ -2,12 +2,9 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
-#include<Servo.h>
-#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
 #include <WiFiClient.h>
-
-#include<stdlib.h>
+#include<Servo.h>
 
 #ifndef STASSID
 #define STASSID "TP-Link_74D0"
@@ -20,14 +17,38 @@
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
+ESP8266WebServer server(80);
 Servo myservo;
+
+char returnString[255];
+
+int nowServoState = 0;
+
+void handleRoot() {
+  sprintf(returnString,"%d\n",nowServoState);
+  server.send(200,"text/plain",returnString);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
   myservo.attach(PIN_SERVO); //モーター制御用ピンとして使用
   pinMode(PIN_LED,OUTPUT); //エラー表示用LEDピンとして使用
   
@@ -35,6 +56,7 @@ void setup() {
     Serial.println("Connection Failed!");
     errorHandling(2);
   }
+  
   ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
@@ -71,34 +93,53 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.begin(115200); //1,2シリアル通信用に使用
-  // Serial.setDebugOutput(true);
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-      Serial.printf("[SETUP] WAIT %d...\n", t);
-      Serial.flush();
-      delay(1000);
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
   }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.on("/open", []() {
+    ctlServo(1);
+    errorHandling(-1);
+    sprintf(returnString,"%d\n",nowServoState);
+    server.send(200,"text/plain",returnString);
+    });
+    
+  server.on("/close", []() {
+    ctlServo(0);
+    errorHandling(-1);
+    sprintf(returnString,"%d\n",nowServoState);
+    server.send(200,"text/plain",returnString);
+    });
+    
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
-unsigned long times = 0;
 void loop() {
   ArduinoOTA.handle();
+  server.handleClient();
+  MDNS.update();
   ctlLed();
   servoOffTimer();
-  if(millis() - times > 10 * 1000){
-        times = millis();
-        polling();
-    }
   if(millis() > 24 * 60 * 60 * 1000) ESP.restart();
 }
 
 int ctlServoTime = 0;
-int nowServoState = 0;
 void ctlServo(int sc){ //サーボ制御タスク
   Serial.printf("[ctlServo] arg:%d\n",sc);
   ctlServoTime = millis();
@@ -122,61 +163,6 @@ void errorHandling(int e){ //エラー処理タスク
   Serial.printf("[errorHandling] arg:%d\n",e);
   if(e == -1) setCtlLed(0,1000);
   else setCtlLed(2,(e+1)*100);
-}
-
-void polling(){ //ポーリングタスク
-    //wifi接続待ち
-    int wstate = WiFi.waitForConnectResult();
-    Serial.printf("WiFIState:%d\n",wstate);
-    if ((wstate == WL_CONNECTED)) {
-        
-        WiFiClient client;
-
-        HTTPClient http;
-
-        Serial.print("[HTTP] begin...\n");
-        if (http.begin(client, "http://192.168.1.46:1919/svctl")) {  // HTTP
-
-
-        Serial.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if (httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println("[polling] var payload:" + payload);
-            //サーボ制御タスク
-            char payloadCharArray[4];
-            payload.toCharArray(payloadCharArray, sizeof payloadCharArray);
-            Serial.printf("[polling] var payloadCharArray:%s\n",payloadCharArray);
-            int svctl = atoi(payloadCharArray);
-            Serial.printf("[polling] var svctl:%d\n",svctl);
-            ctlServo(svctl);
-            errorHandling(-1);
-            }
-        } else {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-            //エラー処理タスク実行
-            errorHandling(0);
-        }
-
-        http.end();
-        } else {
-        Serial.printf("[HTTP} Unable to connect\n");
-        //エラー処理タスク実行
-        errorHandling(1);
-        }
-    }else{
-        //エラー処理タスク実行
-        Serial.println("Connection Failed!");
-        errorHandling(2);
-    }
 }
 
 unsigned long ctlLEDtime = 0;
